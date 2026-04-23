b89becd15dcfd7a92d5bd06e0854ae067ce50a69
vawnthais <bluw.vthai@gmail.com>
2026-04-23 11:53:51 +0700

    Time: 0 ms (100%), Space: 8.4 MB (24.73%) - LeetHub

---
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if (s.length() != goal.length()) {
+            return false;
+        }
+        string str = s + s;
+        return str.find(goal) != string::npos;
+    }
+};
