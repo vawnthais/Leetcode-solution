864ad97f5203ee40a1221f27b69fd52d24aefd3e
vawnthais <bluw.vthai@gmail.com>
2026-04-26 17:30:29 +0700

    Time: 308 ms (6.86%), Space: 69 MB (49.81%) - LeetHub

--- 0595-big-countries/0595-big-countries.py
+++ 0595-big-countries/0595-big-countries.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    df = world.loc[(world['area'] >= 3000000) | (world['population'] >= 25000000), 
-                  ['name', 'population', 'area']]
-    return df
+
+    return world.query("area >= 3000000 or population >= 25000000")[['name', 'population', 'area']]
